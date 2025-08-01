c18abc030a337653ad65c1612b3b24ce0ff37d84
root <root@DP3-2023.localdomain>
2025-08-01 12:10:56 +0400

    Initial React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>🚀 Welcome to the Test React Frontend!</h1>
+      <p>This is triggered from Jenkins Release 🎉</p>
+    </div>
+  );
+}
+
+export default App;
